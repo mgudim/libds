1a91a1bd584ba256672ab1684b98ea165a8978cb
mgudim <mgudim@gmail.com>
2019-09-22 16:20:01 -0400

    Added simple hashing functions.

---
+++ include/Hashing/HashingFunctions.h
@@ -0,0 +1,9 @@
+#ifndef HASHING_FUNCTIONS_INC
+#define HASHING_FUNCTIONS_INC
+
+
+unsigned getModPrime16(int x, unsigned range);
+unsigned char getPearsonHash256(const char* key, unsigned size);
+
+
+#endif
